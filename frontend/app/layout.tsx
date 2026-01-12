ae258f0df8bc362d421a0bc8a220efe47b729194
Tor <torikunpam@gmail.com>
2026-01-13 06:51:49 +0700

    Fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata = {
   title: "Aplikasi Absensi SMK",
   description: "Sistem Absensi Siswa",
+  icons: {
+    icon: "/smk.png", // Ini akan mencari file di folder public atau app
+  },
 };
 
 export default function RootLayout({
